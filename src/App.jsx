713a8f723710ccc72e3f8cf18d9a49c30c3cf662
shin-minhyuk <afs_style@naver.com>
2024-08-07 15:30:47 +0900

    Feat: 기존 css tailwind 형태로 변경, 다크모드, 라이트모드 추가

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,39 @@
+import { useState } from "react";
 import "./App.css";
 import { contents } from "./assets/data/data";
 import Content from "./components/Content";
 import Header from "./components/Header";
+import { createGlobalStyle, css } from "styled-components";
 
 function App() {
+  const [darkMode, setDarkMode] = useState(false);
+
+  const GlobalStyle = createGlobalStyle`
+    ${(props) =>
+      props.darkMode === false
+        ? css`
+            * {
+              color: black;
+              background-color: #fff;
+            }
+
+            #root main header {
+              background-color: #fff;
+            }
+
+            #root main section div span {
+              border: 1px solid green;
+              color: green;
+            }
+          `
+        : null}
+  `;
+
   return (
     <main>
-      <Header />
-      <section>
+      <GlobalStyle darkMode={darkMode} />
+      <Header darkMode={darkMode} setDarkMode={setDarkMode} />
+      <section className="flex flex-wrap justify-center gap-[20px] py-[20px]">
         {contents.map((el) => (
           <Content key={el.id} content={el} />
         ))}
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,19 @@
-export default function Header() {
+import styled, { createGlobalStyle } from "styled-components";
+
+export default function Header({ setDarkMode, darkMode }) {
   return (
-    <header className="header-container">
+    <header className="flex justify-between items-center py-[10px] px-[20px] bg-black">
       <h2>OZ코딩스쿨</h2>
-      <ul className="menu-container">
-        <li>로그인</li>
-        <li>회원가입</li>
-        <li>내클래스</li>
+      <ul className="flex justify-center items-center gap-[20px]">
+        <li className="list-none font-[400] text-[13px]">로그인</li>
+        <li className="list-none font-[400] text-[13px]">회원가입</li>
+        <li className="list-none font-[400] text-[13px]">내클래스</li>
+        <li
+          onClick={() => setDarkMode((prev) => !prev)}
+          className="cursor-pointer border border-red-700 py-[6px] px-[10px]"
+        >
+          {darkMode ? "다크 모드" : "라이트 모드"}
+        </li>
       </ul>
     </header>
   );
